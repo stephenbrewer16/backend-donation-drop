826b5241233bfb04667485faa4957ed8457bd885
Stephen Brewer <stephebrewer16@gmail.com>
2019-12-15 17:10:06 -0500

    create user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,12 @@
 class UserController < ApplicationController
     def create
-        
+         user = User.new(username: params[:username], password: params[:password])
+            if user.save
+                token = encode_token(user.id)
+                render json: {user: UserSerializer.new(user), token: token}
+            else 
+                render json: {errors: user.errors.full_messages}
+        end
     end
 
     def show
