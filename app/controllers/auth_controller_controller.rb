b55dfa5ae91762e2fcc3d0bdfe88fde3f7fd1733
Stephen Brewer <stephebrewer16@gmail.com>
2019-12-15 17:08:20 -0500

    auto login

--- app/controllers/auth_controller_controller.rb
+++ app/controllers/auth_controller_controller.rb
@@ -1,2 +1,18 @@
 class AuthControllerController < ApplicationController
+        def login
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            token = encode_token(user.id)
+            render json: {user: UserSerializer.new(user), token: token}
+        else
+            render json: {errors: "Username or Password is incorrect"}
+        end
+    end
+    def auto_login
+        if session_user
+            render json: session_user
+        else
+            render json: {errors: "No User Logged in"}
+        end
+    end
 end
